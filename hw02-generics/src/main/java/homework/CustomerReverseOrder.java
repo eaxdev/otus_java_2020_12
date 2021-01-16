33d74e66deef1728061fb79055cbf2751a49504a
eaxdev <eaxdev256@gmail.com>
2021-01-16 18:13:19 +0300

    hw02

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,16 +1,21 @@
 package homework;
 
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Queue;
+
 public class CustomerReverseOrder {
 
+    private final Queue<Customer> store = Collections.asLifoQueue(new ArrayDeque<>());
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
 
     public void add(Customer customer) {
-
+        store.add(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return store.poll();
     }
 }
