0cf6eef6ac8be648bd9ecc4248627e4444ec5f8d
Roman S.A <eaxdev256@gmail.com>
2021-02-28 21:45:13 +0300

    Hw4 (#5)
    * hw4
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>
    
    * new analyses
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>
    
    * новые замер с FullGC
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>
    
    * снятие замеров
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>

---
+++ hw04-gc/src/main/java/Benchmark.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Benchmark implements BenchmarkMBean {
+
+    private final List<SomeClass> OBJECTS = new ArrayList<>();
+
+    public void run() {
+        while (true) {
+            for (int i = 0; i < 500_000; i++) {
+                OBJECTS.add(new SomeClass(i, String.valueOf(i), Long.valueOf(i), Character.valueOf((char) i), true, (short) 127));
+            }
+            System.out.println("Size before delete "  + OBJECTS.size());
+            for (int i = 0; i < 5_000; i++) {
+                OBJECTS.remove(i);
+            }
+            System.out.println("Size after delete "  + OBJECTS.size());
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return OBJECTS.size();
+    }
+
+    private static class SomeClass {
+        private final Integer i1;
+        private final String s1;
+        private final Long l1;
+        private final Character c1;
+        private final Boolean b1;
+        private final Short sh1;
+
+        private SomeClass(Integer i1, String s1, Long l1, Character c1, Boolean b1, Short sh1) {
+            this.i1 = i1;
+            this.s1 = s1;
+            this.l1 = l1;
+            this.c1 = c1;
+            this.b1 = b1;
+            this.sh1 = sh1;
+        }
+    }
+}
---
+++ hw04-gc/src/main/java/Gc.java
@@ -0,0 +1,86 @@
+/*
+О формате логов
+http://openjdk.java.net/jeps/158
+
+
+-Xms512m
+-Xmx512m
+-Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
+-XX:+HeapDumpOnOutOfMemoryError
+-XX:HeapDumpPath=./logs/dump
+-XX:+UseG1GC
+*/
+
+/*
+1)
+    default, time: 83 sec (82 without Label_1)
+2)
+    -XX:MaxGCPauseMillis=100000, time: 82 sec //Sets a target for the maximum GC pause time.
+3)
+    -XX:MaxGCPauseMillis=10, time: 91 sec
+
+4)
+-Xms2048m
+-Xmx2048m
+    time: 81 sec
+
+5)
+-Xms5120m
+-Xmx5120m
+    time: 80 sec
+
+5)
+-Xms20480m
+-Xmx20480m
+    time: 81 sec (72 without Label_1)
+
+*/
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.*;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gc {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        switchOnMonitoring();
+        long beginTime = System.currentTimeMillis();
+
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("ru.otus:type=Benchmark");
+
+        Benchmark mbean = new Benchmark();
+        mbs.registerMBean(mbean, name);
+        mbean.run();
+
+        System.out.println("time:" + (System.currentTimeMillis() - beginTime) / 1000);
+    }
+
+    private static void switchOnMonitoring() {
+        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcbean : gcbeans) {
+            System.out.println("GC name:" + gcbean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            NotificationListener listener = (notification, handback) -> {
+                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+
+                    System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
+                }
+            };
+            emitter.addNotificationListener(listener, null, null);
+        }
+    }
+}
