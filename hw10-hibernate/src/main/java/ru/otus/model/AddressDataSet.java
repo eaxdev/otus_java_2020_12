cb225ac1e494a7e2460d5f8656e77f81587a39e1
Roman S.A <eaxdev256@gmail.com>
2021-04-05 23:28:00 +0300

    hw10 (#10)
    * hw10
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>
    
    * fetch eager
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>

---
+++ hw10-hibernate/src/main/java/ru/otus/model/AddressDataSet.java
@@ -0,0 +1,64 @@
+package ru.otus.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address_data_set")
+public class AddressDataSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @JoinColumn(name = "clent_id", referencedColumnName = "id")
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH, orphanRemoval = true)
+    private Client client;
+
+    public AddressDataSet() {
+    }
+
+    public AddressDataSet(Long id, String street) {
+        this.id = id;
+        this.street = street;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    protected AddressDataSet copy() {
+        return new AddressDataSet(this.id, this.street);
+    }
+
+    @Override
+    public String toString() {
+        return "AddressDataSet{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                '}';
+    }
+}
---
+++ hw10-hibernate/src/main/java/ru/otus/model/PhoneDataSet.java
@@ -0,0 +1,68 @@
+package ru.otus.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phone_data_set")
+public class PhoneDataSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public PhoneDataSet() {
+    }
+
+    public PhoneDataSet(String number) {
+        this.number = number;
+    }
+
+    public PhoneDataSet(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public PhoneDataSet copy() {
+        return new PhoneDataSet(this.id, this.number);
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneDataSet{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
