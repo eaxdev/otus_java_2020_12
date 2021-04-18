e97ce26262e9059c50f392b6451e2fcea8417cbe
Roman S.A <eaxdev256@gmail.com>
2021-04-18 14:24:25 +0300

    hw13 (#13)
    * hw13
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>
    
    * исправление замечаний
    
    Signed-off-by: eaxdev <eaxdev256@gmail.com>

--- hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,37 +1,96 @@
 package ru.otus.appcontainer;
 
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
+    public AppComponentsContainerImpl(String packageToScan) {
+        var scanner = new Reflections(packageToScan, new SubTypesScanner(false));
+        var initialConfigClasses = scanner.getSubTypesOf(Object.class)
+                .stream().filter(it -> !it.isInterface() || !it.isAnnotation())
+                .toArray(Class<?>[]::new);
+        process(initialConfigClasses);
+    }
+
     public AppComponentsContainerImpl(Class<?> initialConfigClass) {
-        processConfig(initialConfigClass);
+        process(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
-        checkConfigClass(configClass);
-        // You code here...
+    public AppComponentsContainerImpl(Class<?>... initialConfigClasses) {
+        process(initialConfigClasses);
     }
 
-    private void checkConfigClass(Class<?> configClass) {
-        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
-            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+    private void process(Class<?>... initialConfigClasses) {
+        try {
+            processConfig(initialConfigClasses);
+        } catch (Exception e) {
+            throw new ContainerCreationException("Can't create container", e);
+        }
+    }
+
+    private void processConfig(Class<?>... configClasses) throws Exception {
+        checkConfigClass(configClasses);
+        var configClassesSorted = Arrays.stream(configClasses)
+                .filter(it -> it.isAnnotationPresent(AppComponentsContainerConfig.class))
+                .sorted(Comparator.comparingInt(it -> it.getAnnotation(AppComponentsContainerConfig.class).order()))
+                .collect(Collectors.toList());
+
+        for (var configClass : configClassesSorted) {
+            var sortedMethods = Arrays.stream(configClass.getMethods())
+                    .filter(it -> it.isAnnotationPresent(AppComponent.class))
+                    .sorted(Comparator.comparingInt(it -> it.getAnnotation(AppComponent.class).order()))
+                    .collect(Collectors.toList());
+
+            var instance = configClass.getDeclaredConstructor().newInstance();
+
+            for (var method : sortedMethods) {
+                var parameters = method.getParameterTypes();
+                var args = Arrays.stream(parameters).map(this::findComponent).toArray();
+
+                var component = method.invoke(instance, args);
+
+                var componentName = method.getAnnotation(AppComponent.class).name();
+                appComponentsByName.put(componentName, component);
+                appComponents.add(component);
+            }
+        }
+    }
+
+    private void checkConfigClass(Class<?>[] configClass) {
+        for (Class<?> clazz : configClass) {
+            if (!clazz.isAnnotationPresent(AppComponentsContainerConfig.class)) {
+                throw new IllegalArgumentException(String.format("Given class is not config %s", clazz.getName()));
+            }
         }
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        Object component = findComponent(componentClass);
+        return (C) component;
+    }
+
+    private Object findComponent(Class<?> componentClass) {
+        return appComponents.stream()
+                .filter(componentClass::isInstance)
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("Not found component"));
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) Optional.ofNullable(appComponentsByName.get(componentName))
+                .orElseThrow(() -> new IllegalArgumentException("Not found component"));
     }
 }
---
+++ hw13-di/src/main/java/ru/otus/appcontainer/ContainerCreationException.java
@@ -0,0 +1,8 @@
+package ru.otus.appcontainer;
+
+public class ContainerCreationException extends RuntimeException {
+
+    public ContainerCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
