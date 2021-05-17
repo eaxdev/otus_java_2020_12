1201355425eaddff9ce57fed9389dbe24ae07476
eaxdev <eaxdev256@gmail.com>
2021-05-17 09:52:25 +0300

    hw15
    Signed-off-by: eaxdev <eaxdev256@gmail.com>

---
+++ hw15-threads/src/main/java/ru/otus/DigitPrinter.java
@@ -0,0 +1,35 @@
+package ru.otus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DigitPrinter {
+
+    private static final Logger logger = LoggerFactory.getLogger(DigitPrinter.class);
+
+    private String lastThreadName;
+
+    public DigitPrinter(String lastThreadName) {
+        this.lastThreadName = lastThreadName;
+    }
+
+    public synchronized void print(DigitGenerator generator) {
+        var threadName = Thread.currentThread().getName();
+        try {
+            while (true) {
+
+                while (lastThreadName.equals(threadName)) {
+                    this.wait();
+                }
+
+                lastThreadName = threadName;
+                logger.debug("Thread: {}, print: {}", threadName, generator.generateNext());
+                Thread.sleep(1000);
+                notifyAll();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
